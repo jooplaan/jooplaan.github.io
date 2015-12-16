168bb20b79f8d2a8ef295f62647c96aea339a06c
Joop Laan <joop.laan@gmail.com>
2015-12-16 08:02:58 +0100

    Adding apple touch icons and favicons

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+
+var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men',
+'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
+var show = myShows[Math.floor(Math.random() * myShows.length)];
+
+
+console.log(show);
