da35503d0424e64d8e62624508dea91bbf21ccad
Joop Laan <joop.laan@gmail.com>
2015-12-16 08:21:59 +0100

    Random welcome message

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,13 @@
 
 
-var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men',
-'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
+
+
+var myShows = ['Good morning', 'Goeije morn', 'Guid mornin', 'Bom dia',
+'Bonjour', 'Guten Morgen'];
 var show = myShows[Math.floor(Math.random() * myShows.length)];
 
+var welcomeText = document.getElementById('main-header');
+if (document.getElementById('main-header') != null) {
+  document.getElementById('main-header').innerHTML = show;
+}
 
-console.log(show);
