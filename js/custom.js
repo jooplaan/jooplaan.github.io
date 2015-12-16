df33725883cf889c0214e1d466a571847456e89d
Joop Laan <joop.laan@gmail.com>
2015-12-16 08:27:42 +0100

    Adding comments to JS

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,9 @@
-
-
-
-
+// Random welcome message.
 var myShows = ['Good morning', 'Goeije morn', 'Guid mornin', 'Bom dia',
 'Bonjour', 'Guten Morgen'];
+// Get random welcome.
 var show = myShows[Math.floor(Math.random() * myShows.length)];
-
+// Replace default welcome with random one.
 var welcomeText = document.getElementById('main-header');
 if (document.getElementById('main-header') != null) {
   document.getElementById('main-header').innerHTML = show;
