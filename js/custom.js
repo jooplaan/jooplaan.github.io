736803b60aa1e5de7a82250e82ac51f185e393aa
Joop Laan <joop.laan@gmail.com>
2015-12-16 10:32:42 +0100

    Add welcome message per time of day

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,29 @@
-// Random welcome message.
-var myShows = ['Good morning', 'Goeije morn', 'Guid mornin', 'Bom dia',
-'Bonjour', 'Guten Morgen', 'Goede morgen'];
+// Get hour of the day.
+var d = new Date();
+var h = d.getHours();
+
+// Nighttime.
+if (h > 23 && h < 6) {
+  var myShows = ['Hi nightowl', 'Goeienag', ' Gute Nacht', 'Buenas noches'];
+}
+
+// Morning.
+if (h > 5 && h < 13) {
+  var myShows = ['Good morning', 'Goeije morn', 'Guid mornin', 'Bom dia',
+  'Bonjour', 'Guten Morgen', 'Goede morgen'];
+}
+
+// Afternoon.
+if (h > 12 && h < 18) {
+  var myShows = ['Good afternoon', 'Bona tarda', 'Góðan dag'];
+}
+
+// Evening.
+if (h > 17 && h < 0) {
+  var myShows = ['Good evening', 'Guid evenin', 'Buena nuei', 'Guuten ābend',
+    'Bon nochi', 'Bonsoir', 'Gott kvøld'];
+}
+
 // Get random welcome.
 var show = myShows[Math.floor(Math.random() * myShows.length)];
 // Replace default welcome with random one.
